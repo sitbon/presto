ed4e5f3574bd9c035f6c036cd38cefdca93374e6
Phillip Sitbon <phillip.sitbon@gmail.com>
2022-12-06 21:58:15 -0800

    adict merge improvements and filter function.

--- presto/adict.py
+++ presto/adict.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Union
+from typing import Union, Iterator
 
 from attrdict import AttrDict
 
@@ -12,16 +12,33 @@ class adict(AttrDict):
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}({', '.join(f'{str(key)}={value!r}' for key, value in self.items())})"
 
-    def merged(self: Union[adict, dict], other: dict) -> adict:
-        this = adict(self) if isinstance(self, adict) else dict(self)
-
+    def merge(self: Union[adict, dict], other: dict) -> Union[adict, dict]:
         for key, value in other.items():
-            if key in this and isinstance(this[key], dict) and isinstance(value, dict):
-                this[key] = adict.merged(this[key], value)
+            if key in self and isinstance(self[key], dict) and isinstance(value, dict):
+                self[key] = adict.merge(self[key], value)
             else:
-                this[key] = value
+                self[key] = value
 
-        return this
+        return self
 
-    def merge(self, other: dict) -> None:
-        self.update(self.merged(other))
+    def merged(self: Union[adict, dict], other: dict) -> Union[adict, dict]:
+        this = adict(self) if isinstance(self, adict) else dict(self)
+        return adict.merge(this, other)
+
+    def filter(self: Union[adict, dict], *keys: str, delete=False) -> dict:
+
+        def op(k, v):
+            if k in keys and delete:
+                del self[k]
+
+            if isinstance(v, (adict, dict)):
+                return adict.filter(v, *keys, delete=delete)
+
+            return v
+
+        filtered = {
+            key: op(key, val)
+            for key, val in tuple(self.items())
+        }
+
+        return filtered
