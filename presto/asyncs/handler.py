9d6df0cd5858dae645fc2df55ed1e5ce0cc81b3d
Phillip Sitbon <phillip.sitbon@gmail.com>
2022-12-07 21:24:48 -0800

    use async client as session.

--- presto/asyncs/handler.py
+++ presto/asyncs/handler.py
@@ -1,3 +1,6 @@
+from typing import TypeVar, Self, Optional
+
+from copy import deepcopy
 import httpx
 
 from presto.adict import adict
@@ -8,11 +11,40 @@ from .response import Response
 __all__ = "Handler",
 
 
+PrestoT = TypeVar("PrestoT", bound="Presto")
+
+
 class Handler(Presto.Handler):
+    _client: httpx.AsyncClient
+
+    def __init__(
+            self,
+            presto: PrestoT,
+    ):
+        self._presto = presto
+        self._client = httpx.AsyncClient()
 
     async def __call__(self, request: Presto.Request, **kwds) -> Presto.Response:
         url = request.__url__
         params = adict(request.__request__).__merge__(kwds)
         method = params.pop("method")
-        async with httpx.AsyncClient() as client:
-            return Response(await client.request(method, url, **params))
+        return Response(await self._client.request(method, url, **params))
+
+    @property
+    def client(self):
+        return self._client
+
+    def __copy__(self) -> Self:
+        this = self.__class__.__new__(self.__class__)
+        this._presto = self._presto
+        this._client = self._client
+        return this
+
+    def __deepcopy__(self, memo: dict, to: Optional[PrestoT] = None) -> Self:
+        if id(self) in memo:
+            return memo[id(self)]  # For when Presto calls this directly.
+        this = self.__class__.__new__(self.__class__)
+        memo[id(self)] = this
+        this._presto = to or deepcopy(self._presto, memo)
+        this._client = httpx.AsyncClient()
+        return this
