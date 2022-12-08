1da0f0f32d7e1624990fa7718e936b08e1c2e807
Phillip Sitbon <phillip.sitbon@gmail.com>
2022-12-07 16:36:11 -0800

    response: more exception handling options in response class.

--- presto/response.py
+++ presto/response.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Optional
+from typing import Optional, Container
 
 import requests
 
@@ -10,12 +10,24 @@ __all__ = "_Response",
 
 
 class _Response(requests.Response):
+    _RAISE_FOR_STATUS: bool = True
+    _RAISE_EXCEPT_FOR: Container = set()
+
     attr: Optional[adict] = None
 
     # noinspection PyMissingConstructor
     def __init__(self, response: requests.Response):
         self.__dict__ = response.__dict__
-        self.raise_for_status()
+
+        if self._RAISE_FOR_STATUS is True:
+            self.raise_for_status()
 
         if self.ok and self.headers.get("content-type").startswith("application/json"):
             self.attr = adict(self.json())
+
+    def _raise_for_status(self):
+        return super().raise_for_status()
+
+    def raise_for_status(self):
+        if self.status_code not in self._RAISE_EXCEPT_FOR:
+            self._raise_for_status()
